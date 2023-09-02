66da017dbde3faed19d7bace331f162dc6f6498d
vladbalea <vladd.balea@gmail.com>
2023-09-02 22:30:37 +0300

    Fixed weird dialog callback bug

--- src/features/dialog.ts
+++ src/features/dialog.ts
@@ -5,17 +5,17 @@ type DialogResponseCallback = (response: boolean, listItem: number, inputText: s
 const dialogCallbackMap = new Map<number, DialogResponseCallback>()
 
 SampNode.on("OnDialogResponse", (playerId: number, dialogId: number, response: number, listItem: number, inputText: string) => {
-    const callback = dialogCallbackMap.get(playerId)
-    if (callback) {
+    const callback = dialogCallbackMap.get(dialogId)
+    if (callback !== undefined) {
         callback(response === 1, listItem, inputText)
-        dialogCallbackMap.delete(playerId)
+        dialogCallbackMap.delete(dialogId)
     }
 })
 
 export function showPlayerDialog(playerId: number, styleId: DialogStyleEnum, caption: string, info: string, button1: string, button2: string, callback: DialogResponseCallback) {
-    dialogCallbackMap.set(playerId, callback)
-
-    return Natives.showPlayerDialog(playerId, 0, styleId, caption, info, button1, button2)
+    const dialogId = Math.floor(Math.random() * 32767)
+    dialogCallbackMap.set(dialogId, callback)
+    return Natives.showPlayerDialog(playerId, dialogId, styleId, caption, info, button1, button2)
 }
 
 export function hidePlayerDialog(playerId: number) {
