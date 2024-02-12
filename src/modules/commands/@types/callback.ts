9a80d75c0896c45b574dd4e180c87c84313d89b9
vladgitx <vladd.balea@gmail.com>
2024-02-12 16:10:20 +0200

    A command param can also be undefined

--- src/modules/commands/@types/callback.ts
+++ src/modules/commands/@types/callback.ts
@@ -1,3 +1,3 @@
 import { PlayerMp } from "../../player"
 
-export type CommandCallback = (player: PlayerMp, ...params: string[]) => void
+export type CommandCallback = (player: PlayerMp, ...params: (string | undefined)[]) => void
