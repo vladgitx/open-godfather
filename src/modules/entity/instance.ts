61f2d2f8531b10eb396e0cf3d9e2f3a4ce8071ad
vladgitx <vladd.balea@gmail.com>
2024-04-06 16:18:04 +0300

    Added onCleanup to entities

--- src/modules/entity/instance.ts
+++ src/modules/entity/instance.ts
@@ -2,8 +2,13 @@ import { CONFIG } from "../../shared/config"
 
 export class Entity {
     private variables = new Map<string, any>()
+    private cleanupCallbacks: (() => void)[] = []
 
     constructor(public id: number) {}
+
+    onCleanup(callback: () => void) {
+        this.cleanupCallbacks.push(callback)
+    }
 
     setVariable(name: string, value: any) {
         this.variables.set(name, value)
@@ -17,11 +22,16 @@ export class Entity {
         return this.variables.delete(name)
     }
 
-    set exists(value: boolean) {
+    set exists(value: false) {
+        for (const callback of this.cleanupCallbacks) {
+            callback()
+        }
+        this.cleanupCallbacks = []
+        
         this.id = CONFIG.entity.invalidId
     }
 
-    get exists() {
+    get exists(): boolean {
         return this.id !== CONFIG.entity.invalidId
     }
 }
