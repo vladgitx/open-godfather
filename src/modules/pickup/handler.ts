4878ae217737dd614d9724bc17bed8073f3d5bb3
vladgitx <vladd.balea@gmail.com>
2024-04-23 22:00:49 +0300

    Adapt PickupHandler to StreamerHandler

--- src/modules/pickup/handler.ts
+++ src/modules/pickup/handler.ts
@@ -2,8 +2,9 @@ import { streamerNatives } from "@/natives/streamer"
 import { type Vector3 } from "../vector3"
 import { pickupFactory } from "./factory"
 import { type Pickup } from "./instance"
+import { StreamerHandler } from "../streamer-entity"
 
-class PickupHandler {
+class PickupHandler extends StreamerHandler<Pickup> {
     new(model: number, position: Vector3, streamDistance = 200, world = -1, interior = -1) {
         const pickupId = streamerNatives.createDynamicPickup(model, 1, position, world, interior, -1, streamDistance, -1, 0)
 
@@ -24,4 +25,4 @@ class PickupHandler {
     }
 }
 
-export const pickupHandler = new PickupHandler()
+export const pickupHandler = new PickupHandler("pickup", (id) => pickupFactory.pool.get(id))
