7637e84ff47fdd6ba1823805a360163d8cadc0bc
vladgitx <vladd.balea@gmail.com>
2024-04-30 00:41:47 +0300

    Add more scripting API to player att. objects

--- src/modules/player/attached-objects/entity.ts
+++ src/modules/player/attached-objects/entity.ts
@@ -5,22 +5,108 @@ import { nativeFunctions } from "@/natives"
 import { type Player } from "../entity"
 
 export class PlayerAttachedObject extends Entity {
+    private _bone: PlayerBonesEnum
+    private _firstMaterialColor: string
+    private _secondMaterialColor: string
+
     constructor(
         private player: Player,
         slot: number,
         readonly model: number,
-        readonly bone: PlayerBonesEnum,
+        bone: PlayerBonesEnum,
         offset: Vector3,
         rotation: Vector3,
         scale: Vector3,
-        readonly firstMaterialColor: string,
-        readonly secondMaterialColor: string,
+        firstMaterialColor: string,
+        secondMaterialColor: string,
     ) {
         super(slot)
 
         this.setVariable("playerAttObj::internal::offset", offset)
         this.setVariable("playerAttObj::internal::rotation", rotation)
         this.setVariable("playerAttObj::internal::scale", scale)
+
+        this._bone = bone
+        this._firstMaterialColor = firstMaterialColor
+        this._secondMaterialColor = secondMaterialColor
+    }
+
+    set firstMaterialColor(value: string) {
+        this._firstMaterialColor = value
+
+        nativeFunctions.setPlayerAttachedObject(
+            this.player.id,
+            this.id,
+            this.model,
+            this.bone,
+            this.offset.x,
+            this.offset.y,
+            this.offset.z,
+            this.rotation.x,
+            this.rotation.y,
+            this.rotation.z,
+            this.scale.x,
+            this.scale.y,
+            this.scale.z,
+            value,
+            this.secondMaterialColor,
+        )
+    }
+
+    get firstMaterialColor() {
+        return this._firstMaterialColor
+    }
+
+    set secondMaterialColor(value: string) {
+        this._secondMaterialColor = value
+
+        nativeFunctions.setPlayerAttachedObject(
+            this.player.id,
+            this.id,
+            this.model,
+            this.bone,
+            this.offset.x,
+            this.offset.y,
+            this.offset.z,
+            this.rotation.x,
+            this.rotation.y,
+            this.rotation.z,
+            this.scale.x,
+            this.scale.y,
+            this.scale.z,
+            this.firstMaterialColor,
+            value,
+        )
+    }
+
+    get secondMaterialColor() {
+        return this._secondMaterialColor
+    }
+
+    set bone(value: PlayerBonesEnum) {
+        this._bone = value
+
+        nativeFunctions.setPlayerAttachedObject(
+            this.player.id,
+            this.id,
+            this.model,
+            value,
+            this.offset.x,
+            this.offset.y,
+            this.offset.z,
+            this.rotation.x,
+            this.rotation.y,
+            this.rotation.z,
+            this.scale.x,
+            this.scale.y,
+            this.scale.z,
+            this.firstMaterialColor,
+            this.secondMaterialColor,
+        )
+    }
+
+    get bone() {
+        return this._bone
     }
 
     set offset(value: Vector3) {
