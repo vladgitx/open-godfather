3041ff75ceda667d95b191ee75407dcb99d8321f
vladgitx <vladd.balea@gmail.com>
2024-10-21 19:48:56 +0300

    Add playerShoot palyer event

--- src/components/player/@events/entity-event-bus.ts
+++ src/components/player/@events/entity-event-bus.ts
@@ -2,7 +2,8 @@ import type { Vehicle } from "@/components/vehicle"
 import { dispatcher } from "@/lib/dispatcher"
 import { EventBus } from "@/lib/event-bus"
 import { type Vector3 } from "@/lib/vector3"
-import type { PlayerState } from "@/wrapper/game/enums.public"
+import type { PlayerState, Weapon } from "@/wrapper/game/enums.public"
+import { type Player } from "../entity"
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export type PlayerEventMap = {
@@ -11,7 +12,12 @@ export type PlayerEventMap = {
     exitVehicle: [Vehicle | undefined]
     editAttachedObject: [number, number, number, Vector3, Vector3, Vector3]
     cancelObjectEditMode: []
+    playerShoot: [Weapon, Player | Vehicle | undefined, Vector3]
 }
+
+dispatcher.on("playerShoot", (player, weapon, target, hitPosition) => {
+    EventBus.emit(player.events, "playerShoot", weapon, target, hitPosition)
+})
 
 dispatcher.on("playerStateChange", (player, newState, oldState) => {
     EventBus.emit(player.events, "stateChange", newState, oldState)
