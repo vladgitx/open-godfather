2f9cdeba06362cb09d0c11982a00f0c4d260841d
vlad <me@vlad.com>
2025-10-03 15:51:13 +0300

    Add EventBus.setListener

--- src/lib/event-bus.ts
+++ src/lib/event-bus.ts
@@ -1,21 +1,22 @@
 export type EventMapInterface = Record<string | symbol | number, unknown[]>
 
 export class EventBus<EventMap extends EventMapInterface = EventMapInterface> {
-    private callbacks = new Map<keyof EventMap, ((...args: unknown[]) => void)[]>()
+    private callbacks = new Map<keyof EventMap, Map<string | symbol, (...args: unknown[]) => void>>()
 
     on<K extends keyof EventMap>(eventName: K, callback: (...args: EventMap[K]) => void) {
-        if (!this.callbacks.has(eventName)) {
-            this.callbacks.set(eventName, [])
-        }
+        return this.setListener(eventName, Symbol(), callback)
+    }
 
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        this.callbacks.get(eventName)!.push(callback as (...args: unknown[]) => void)
+    setListener<K extends keyof EventMap>(eventName: K, id: string | symbol, callback: (...args: EventMap[K]) => void) {
+        const keyedCallbacks = (this.callbacks.get(eventName) ?? new Map<string | symbol, (...args: unknown[]) => void>()).set(
+            id,
+            callback as (...args: unknown[]) => void,
+        )
+
+        this.callbacks.set(eventName, keyedCallbacks)
 
         return () => {
-            this.callbacks.set(
-                eventName,
-                (this.callbacks.get(eventName) ?? []).filter((cb) => cb !== callback),
-            )
+            this.callbacks.get(eventName)?.delete(id)
         }
     }
 
@@ -24,13 +25,13 @@ export class EventBus<EventMap extends EventMapInterface = EventMapInterface> {
         eventName: K,
         ...args: EventMap[K]
     ) {
-        const callbacks = eventBus.callbacks.get(eventName) ?? []
+        const callbacks = eventBus.callbacks.get(eventName) ?? new Map<string | symbol, (...args: unknown[]) => void>()
 
-        for (const callback of callbacks) {
+        for (const [id, callback] of callbacks) {
             try {
                 callback(...args)
             } catch (error) {
-                console.error(`Error while emitting "${String(eventName)}" event for entity`, error)
+                console.error(`[EventBus] Error while emitting "${String(eventName)}" event with ID ${String(id)}`, error)
             }
         }
     }
